1cae2d2cfa02d0c04c02f60009eb418ee55f9672
Lizzie Chan <lchan217@gmail.com>
2019-03-06 14:27:00 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, details|
     details.each do |detail, data|
-      binding.pry
-      data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      :favorite_ice_cream_flavorsdata.delete_if {|ice_cream| ice_cream == "strawberry"}
     end 
   end 
 end
